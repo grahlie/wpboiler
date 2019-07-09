467831bbac36d476d46d2292c18a15f79128ed3a
grahlie <mathias@grahlie.se>
2019-07-09 18:46:57 +0200

    creating htaccess and wp-config

--- .scripts/create_htaccess.sh
+++ .scripts/create_htaccess.sh
@@ -3,70 +3,71 @@
 echo "==============="
 echo "Running $(basename $0)"
 echo "==============="
-
 FILE=$1
 source ./parse-config.sh $FILE
 
-if [ "$MULTISITE" -gt 0 ]; then
-    echo "# BEGIN WordPress"                                                            >>../$FOLDER/wp/.htaccess
-    echo "<IfModule mod_rewrite.c>"                                                     >>../$FOLDER/wp/.htaccess
-    echo "RewriteEngine On"                                                             >>../$FOLDER/wp/.htaccess
-    echo "RewriteBase /"                                                                >>../$FOLDER/wp/.htaccess
-    echo "RewriteRule ^server-status - [L]"                                             >>../$FOLDER/wp/.htaccess
-    echo "RewriteRule ^index\.php$ - [L]"                                               >>../$FOLDER/wp/.htaccess
-    echo "# add a trailing slash to /wp-admin"                                          >>../$FOLDER/wp/.htaccess
-    echo "RewriteRule ^([_0-9a-zA-Z-]+/)?wp-admin$ $1wp-admin/ [R=301,L]"               >>../$FOLDER/wp/.htaccess
-    echo "RewriteCond %{REQUEST_FILENAME} -f [OR]"                                      >>../$FOLDER/wp/.htaccess
-    echo "RewriteCond %{REQUEST_FILENAME} -d"                                           >>../$FOLDER/wp/.htaccess
-    echo "RewriteRule ^ - [L]"                                                          >>../$FOLDER/wp/.htaccess
-    echo "RewriteRule ^([_0-9a-zA-Z-]+/)?(wp-(content|admin|includes).*) $2 [L]"        >>../$FOLDER/wp/.htaccess
-    echo "RewriteRule ^([_0-9a-zA-Z-]+/)?(.*\.php)$ $2 [L]"                             >>../$FOLDER/wp/.htaccess
-    echo "RewriteRule . index.php [L]"                                                  >>../$FOLDER/wp/.htaccess
-    echo "</IfModule>"                                                                  >>../$FOLDER/wp/.htaccess
-    echo "# END WordPress"                                                              >>../$FOLDER/wp/.htaccess
-else
-    echo "# BEGIN WordPress"                                                            >>../$FOLDER/wp/.htaccess
-    echo "<IfModule mod_rewrite.c>"                                                     >>../$FOLDER/wp/.htaccess
-    echo "RewriteEngine On"                                                             >>../$FOLDER/wp/.htaccess
-    echo "RewriteBase /"                                                                >>../$FOLDER/wp/.htaccess
-    echo "RewriteRule ^server-status - [L]"                                             >>../$FOLDER/wp/.htaccess
-    echo "RewriteRule ^index\.php$ - [L]"                                               >>../$FOLDER/wp/.htaccess
-    echo "RewriteCond %{REQUEST_FILENAME} !-f"                                          >>../$FOLDER/wp/.htaccess
-    echo "RewriteCond %{REQUEST_FILENAME} !-d"                                          >>../$FOLDER/wp/.htaccess
-    echo "RewriteRule . /index.php [L]"                                                 >>../$FOLDER/wp/.htaccess
-    echo "</IfModule>"                                                                  >>../$FOLDER/wp/.htaccess
-    echo "# END WordPress"                                                              >>../$FOLDER/wp/.htaccess
+if [[ ! -f "../$FOLDER/wp/.htaccess" ]]; then
+    if [ "$MULTISITE" -gt 0 ]; then
+        echo "# BEGIN WordPress"                                                            >>../$FOLDER/wp/.htaccess
+        echo "<IfModule mod_rewrite.c>"                                                     >>../$FOLDER/wp/.htaccess
+        echo "RewriteEngine On"                                                             >>../$FOLDER/wp/.htaccess
+        echo "RewriteBase /"                                                                >>../$FOLDER/wp/.htaccess
+        echo "RewriteRule ^server-status - [L]"                                             >>../$FOLDER/wp/.htaccess
+        echo "RewriteRule ^index\.php$ - [L]"                                               >>../$FOLDER/wp/.htaccess
+        echo "# add a trailing slash to /wp-admin"                                          >>../$FOLDER/wp/.htaccess
+        echo "RewriteRule ^([_0-9a-zA-Z-]+/)?wp-admin$ $1wp-admin/ [R=301,L]"               >>../$FOLDER/wp/.htaccess
+        echo "RewriteCond %{REQUEST_FILENAME} -f [OR]"                                      >>../$FOLDER/wp/.htaccess
+        echo "RewriteCond %{REQUEST_FILENAME} -d"                                           >>../$FOLDER/wp/.htaccess
+        echo "RewriteRule ^ - [L]"                                                          >>../$FOLDER/wp/.htaccess
+        echo "RewriteRule ^([_0-9a-zA-Z-]+/)?(wp-(content|admin|includes).*) $2 [L]"        >>../$FOLDER/wp/.htaccess
+        echo "RewriteRule ^([_0-9a-zA-Z-]+/)?(.*\.php)$ $2 [L]"                             >>../$FOLDER/wp/.htaccess
+        echo "RewriteRule . index.php [L]"                                                  >>../$FOLDER/wp/.htaccess
+        echo "</IfModule>"                                                                  >>../$FOLDER/wp/.htaccess
+        echo "# END WordPress"                                                              >>../$FOLDER/wp/.htaccess
+    else
+        echo "# BEGIN WordPress"                                                            >>../$FOLDER/wp/.htaccess
+        echo "<IfModule mod_rewrite.c>"                                                     >>../$FOLDER/wp/.htaccess
+        echo "RewriteEngine On"                                                             >>../$FOLDER/wp/.htaccess
+        echo "RewriteBase /"                                                                >>../$FOLDER/wp/.htaccess
+        echo "RewriteRule ^server-status - [L]"                                             >>../$FOLDER/wp/.htaccess
+        echo "RewriteRule ^index\.php$ - [L]"                                               >>../$FOLDER/wp/.htaccess
+        echo "RewriteCond %{REQUEST_FILENAME} !-f"                                          >>../$FOLDER/wp/.htaccess
+        echo "RewriteCond %{REQUEST_FILENAME} !-d"                                          >>../$FOLDER/wp/.htaccess
+        echo "RewriteRule . /index.php [L]"                                                 >>../$FOLDER/wp/.htaccess
+        echo "</IfModule>"                                                                  >>../$FOLDER/wp/.htaccess
+        echo "# END WordPress"                                                              >>../$FOLDER/wp/.htaccess
+    fi
+
+
+    echo "# Config added from code"                                                         >>../$FOLDER/wp/.htaccess
+    echo "<IfModule mod_deflate.c>"                                                         >>../$FOLDER/wp/.htaccess
+    echo "  # Compress HTML, CSS, JavaScript, Text, XML and fonts"                          >>../$FOLDER/wp/.htaccess
+    echo "  AddOutputFilterByType DEFLATE application/javascript"                           >>../$FOLDER/wp/.htaccess
+    echo "  AddOutputFilterByType DEFLATE application/rss+xml"                              >>../$FOLDER/wp/.htaccess
+    echo "  AddOutputFilterByType DEFLATE application/vnd.ms-fontobject"                    >>../$FOLDER/wp/.htaccess
+    echo "  AddOutputFilterByType DEFLATE application/x-font"                               >>../$FOLDER/wp/.htaccess
+    echo "  AddOutputFilterByType DEFLATE application/x-font-opentype"                      >>../$FOLDER/wp/.htaccess
+    echo "  AddOutputFilterByType DEFLATE application/x-font-otf"                           >>../$FOLDER/wp/.htaccess
+    echo "  AddOutputFilterByType DEFLATE application/x-font-truetype"                      >>../$FOLDER/wp/.htaccess
+    echo "  AddOutputFilterByType DEFLATE application/x-font-ttf"                           >>../$FOLDER/wp/.htaccess
+    echo "  AddOutputFilterByType DEFLATE application/x-javascript"                         >>../$FOLDER/wp/.htaccess
+    echo "  AddOutputFilterByType DEFLATE application/xhtml+xml"                            >>../$FOLDER/wp/.htaccess
+    echo "  AddOutputFilterByType DEFLATE application/xml"                                  >>../$FOLDER/wp/.htaccess
+    echo "  AddOutputFilterByType DEFLATE font/opentype"                                    >>../$FOLDER/wp/.htaccess
+    echo "  AddOutputFilterByType DEFLATE font/otf"                                         >>../$FOLDER/wp/.htaccess
+    echo "  AddOutputFilterByType DEFLATE font/ttf"                                         >>../$FOLDER/wp/.htaccess
+    echo "  AddOutputFilterByType DEFLATE image/svg+xml"                                    >>../$FOLDER/wp/.htaccess
+    echo "  AddOutputFilterByType DEFLATE image/x-icon"                                     >>../$FOLDER/wp/.htaccess
+    echo "  AddOutputFilterByType DEFLATE text/css"                                         >>../$FOLDER/wp/.htaccess
+    echo "  AddOutputFilterByType DEFLATE text/html"                                        >>../$FOLDER/wp/.htaccess
+    echo "  AddOutputFilterByType DEFLATE text/javascript"                                  >>../$FOLDER/wp/.htaccess
+    echo "  AddOutputFilterByType DEFLATE text/plain"                                       >>../$FOLDER/wp/.htaccess
+    echo "  AddOutputFilterByType DEFLATE text/xml"                                         >>../$FOLDER/wp/.htaccess
+    echo ""                                                                                 >>../$FOLDER/wp/.htaccess
+    echo "  # Remove browser bugs (only needed for really old browsers)"                    >>../$FOLDER/wp/.htaccess
+    echo "  BrowserMatch ^Mozilla/4 gzip-only-text/html"                                    >>../$FOLDER/wp/.htaccess
+    echo "  BrowserMatch ^Mozilla/4\.0[678] no-gzip"                                        >>../$FOLDER/wp/.htaccess
+    echo "  BrowserMatch \bMSIE !no-gzip !gzip-only-text/html"                              >>../$FOLDER/wp/.htaccess
+    echo "  Header append Vary User-Agent"                                                  >>../$FOLDER/wp/.htaccess
+    echo "</IfModule>"                                                                      >>../$FOLDER/wp/.htaccess
 fi
-
-
-echo "# Config added from code"                                                         >>../$FOLDER/wp/.htaccess
-echo "<IfModule mod_deflate.c>"                                                         >>../$FOLDER/wp/.htaccess
-echo "  # Compress HTML, CSS, JavaScript, Text, XML and fonts"                          >>../$FOLDER/wp/.htaccess
-echo "  AddOutputFilterByType DEFLATE application/javascript"                           >>../$FOLDER/wp/.htaccess
-echo "  AddOutputFilterByType DEFLATE application/rss+xml"                              >>../$FOLDER/wp/.htaccess
-echo "  AddOutputFilterByType DEFLATE application/vnd.ms-fontobject"                    >>../$FOLDER/wp/.htaccess
-echo "  AddOutputFilterByType DEFLATE application/x-font"                               >>../$FOLDER/wp/.htaccess
-echo "  AddOutputFilterByType DEFLATE application/x-font-opentype"                      >>../$FOLDER/wp/.htaccess
-echo "  AddOutputFilterByType DEFLATE application/x-font-otf"                           >>../$FOLDER/wp/.htaccess
-echo "  AddOutputFilterByType DEFLATE application/x-font-truetype"                      >>../$FOLDER/wp/.htaccess
-echo "  AddOutputFilterByType DEFLATE application/x-font-ttf"                           >>../$FOLDER/wp/.htaccess
-echo "  AddOutputFilterByType DEFLATE application/x-javascript"                         >>../$FOLDER/wp/.htaccess
-echo "  AddOutputFilterByType DEFLATE application/xhtml+xml"                            >>../$FOLDER/wp/.htaccess
-echo "  AddOutputFilterByType DEFLATE application/xml"                                  >>../$FOLDER/wp/.htaccess
-echo "  AddOutputFilterByType DEFLATE font/opentype"                                    >>../$FOLDER/wp/.htaccess
-echo "  AddOutputFilterByType DEFLATE font/otf"                                         >>../$FOLDER/wp/.htaccess
-echo "  AddOutputFilterByType DEFLATE font/ttf"                                         >>../$FOLDER/wp/.htaccess
-echo "  AddOutputFilterByType DEFLATE image/svg+xml"                                    >>../$FOLDER/wp/.htaccess
-echo "  AddOutputFilterByType DEFLATE image/x-icon"                                     >>../$FOLDER/wp/.htaccess
-echo "  AddOutputFilterByType DEFLATE text/css"                                         >>../$FOLDER/wp/.htaccess
-echo "  AddOutputFilterByType DEFLATE text/html"                                        >>../$FOLDER/wp/.htaccess
-echo "  AddOutputFilterByType DEFLATE text/javascript"                                  >>../$FOLDER/wp/.htaccess
-echo "  AddOutputFilterByType DEFLATE text/plain"                                       >>../$FOLDER/wp/.htaccess
-echo "  AddOutputFilterByType DEFLATE text/xml"                                         >>../$FOLDER/wp/.htaccess
-echo ""                                                                                 >>../$FOLDER/wp/.htaccess
-echo "  # Remove browser bugs (only needed for really old browsers)"                    >>../$FOLDER/wp/.htaccess
-echo "  BrowserMatch ^Mozilla/4 gzip-only-text/html"                                    >>../$FOLDER/wp/.htaccess
-echo "  BrowserMatch ^Mozilla/4\.0[678] no-gzip"                                        >>../$FOLDER/wp/.htaccess
-echo "  BrowserMatch \bMSIE !no-gzip !gzip-only-text/html"                              >>../$FOLDER/wp/.htaccess
-echo "  Header append Vary User-Agent"                                                  >>../$FOLDER/wp/.htaccess
-echo "</IfModule>"                                                                      >>../$FOLDER/wp/.htaccess
--- .scripts/create_wpconfig.sh
+++ .scripts/create_wpconfig.sh
@@ -7,69 +7,71 @@ echo "==============="
 FILE=$1
 source ./parse-config.sh $FILE
 
-echo "<?php"                                           >>../$FOLDER/wp/wp-config.php
-# Setup multisite
-if [ "$MULTISITE" -gt 0 ]; then
-    echo "define('WP_ALLOW_MULTISITE', true);"         >>../$FOLDER/wp/wp-config.php
-    echo "define('MULTISITE', true);"                  >>../$FOLDER/wp/wp-config.php
-    echo "define('SUBDOMAIN_INSTALL', false);"         >>../$FOLDER/wp/wp-config.php
-    echo "define('PATH_CURRENT_SITE', '/');"           >>../$FOLDER/wp/wp-config.php
-    echo "define('SITE_ID_CURRENT_SITE', 1);"          >>../$FOLDER/wp/wp-config.php
-    echo "define('BLOG_ID_CURRENT_SITE', 1);"          >>../$FOLDER/wp/wp-config.php
+if [[ ! -f "../$FOLDER/wp/wp-config.php" ]]; then
+    echo "<?php"                                           >>../$FOLDER/wp/wp-config.php
+    # Setup multisite
+    if [ "$MULTISITE" -gt 0 ]; then
+        echo "define('WP_ALLOW_MULTISITE', true);"         >>../$FOLDER/wp/wp-config.php
+        echo "define('MULTISITE', true);"                  >>../$FOLDER/wp/wp-config.php
+        echo "define('SUBDOMAIN_INSTALL', false);"         >>../$FOLDER/wp/wp-config.php
+        echo "define('PATH_CURRENT_SITE', '/');"           >>../$FOLDER/wp/wp-config.php
+        echo "define('SITE_ID_CURRENT_SITE', 1);"          >>../$FOLDER/wp/wp-config.php
+        echo "define('BLOG_ID_CURRENT_SITE', 1);"          >>../$FOLDER/wp/wp-config.php
+    fi
+
+    # Debug mode
+    if [ "$DEBUG" -gt 0 ]; then
+        echo "define('WP_DEBUG', true);"                   >>../$FOLDER/wp/wp-config.php
+        echo "define('WP_DEBUG_LOG', true);"               >>../$FOLDER/wp/wp-config.php
+        echo "define('WP_DEBUG_DISPLAY', false);"          >>../$FOLDER/wp/wp-config.php
+    else
+        echo "define('WP_DEBUG', false);"                  >>../$FOLDER/wp/wp-config.php
+        echo "define('WP_DEBUG_LOG', false);"              >>../$FOLDER/wp/wp-config.php
+        echo "define('WP_DEBUG_DISPLAY', true);"           >>../$FOLDER/wp/wp-config.php
+    fi
+
+    echo ""                                                >>../$FOLDER/wp/wp-config.php
+
+    # if [ "$HTTPS" -gt 0 ]; then
+    #     echo "define('FORCE_SSL_ADMIN', true);"            >>../$FOLDER/wp/wp-config.php
+    # fi
+
+    echo ""                                                >>../$FOLDER/wp/wp-config.php
+
+    # Setup database
+    echo "define('DB_NAME', '"$DBNAME"');"                 >>../$FOLDER/wp/wp-config.php
+    echo "define('DB_USER', '"$DBUSER"');"                 >>../$FOLDER/wp/wp-config.php
+    echo "define('DB_PASSWORD', '"$DBPASS"');"             >>../$FOLDER/wp/wp-config.php
+    echo "define('DB_HOST', '"$NAME"-db');"                >>../$FOLDER/wp/wp-config.php
+    echo "define('WP_SITEURL', 'http://"$DOMAIN"');"       >>../$FOLDER/wp/wp-config.php
+    echo "define('WP_HOME', 'http://"$DOMAIN"');"          >>../$FOLDER/wp/wp-config.php
+    echo "\$table_prefix = 's3curityy_gr4hl13_';"          >>../$FOLDER/wp/wp-config.php
+
+    echo ""                                                >>../$FOLDER/wp/wp-config.php
+
+    # Set salts
+    curl -s https://api.wordpress.org/secret-key/1.1/salt/ >>../$FOLDER/wp/wp-config.php
+
+    echo "define('WP_DEFAULT_THEME', 'grahlie');"          >>../$FOLDER/wp/wp-config.php
+    echo "define('DB_CHARSET', 'utf8mb4');"                >>../$FOLDER/wp/wp-config.php
+    echo "define('DB_COLLATE', '');"                       >>../$FOLDER/wp/wp-config.php
+    echo "define('FS_METHOD', 'direct');"                  >>../$FOLDER/wp/wp-config.php
+
+    echo "# Disable edits & updates"                       >>../$FOLDER/wp/wp-config.php
+    echo "define('DISALLOW_FILE_EDIT', true);"             >>../$FOLDER/wp/wp-config.php
+    echo "define('DISALLOW_FILE_MODS', true);"             >>../$FOLDER/wp/wp-config.php
+    echo "define('AUTOMATIC_UPDATER_DISABLED', true);"     >>../$FOLDER/wp/wp-config.php
+    echo "define('WP_AUTO_UPDATE_CORE', false);"           >>../$FOLDER/wp/wp-config.php
+
+    echo "# Other settings"                                >>../$FOLDER/wp/wp-config.php
+    echo "define('WP_AUTO_UPDATE_CORE', false);"           >>../$FOLDER/wp/wp-config.php
+    echo "define('AUTOMATIC_UPDATER_DISABLED', true);"     >>../$FOLDER/wp/wp-config.php
+    echo "define('DISALLOW_FILE_EDIT', true);"             >>../$FOLDER/wp/wp-config.php
+    echo "define('WP_MEMORY_LIMIT', '96M');"               >>../$FOLDER/wp/wp-config.php
+    echo "define('WP_POST_REVISIONS', 3);"                 >>../$FOLDER/wp/wp-config.php
+
+    echo "if ( !defined('ABSPATH') )"                      >>../$FOLDER/wp/wp-config.php
+    echo "  define('ABSPATH', dirname(__FILE__) . '/');"   >>../$FOLDER/wp/wp-config.php
+
+    echo "require_once(ABSPATH . 'wp-settings.php');"      >>../$FOLDER/wp/wp-config.php
 fi
-
-# Debug mode
-if [ "$DEBUG" -gt 0 ]; then
-    echo "define('WP_DEBUG', true);"                   >>../$FOLDER/wp/wp-config.php
-    echo "define('WP_DEBUG_LOG', true);"               >>../$FOLDER/wp/wp-config.php
-    echo "define('WP_DEBUG_DISPLAY', false);"          >>../$FOLDER/wp/wp-config.php
-else
-    echo "define('WP_DEBUG', false);"                  >>../$FOLDER/wp/wp-config.php
-    echo "define('WP_DEBUG_LOG', false);"              >>../$FOLDER/wp/wp-config.php
-    echo "define('WP_DEBUG_DISPLAY', true);"           >>../$FOLDER/wp/wp-config.php
-fi
-
-echo ""                                                >>../$FOLDER/wp/wp-config.php
-
-# if [ "$HTTPS" -gt 0 ]; then
-#     echo "define('FORCE_SSL_ADMIN', true);"            >>../$FOLDER/wp/wp-config.php
-# fi
-
-echo ""                                                >>../$FOLDER/wp/wp-config.php
-
-# Setup database
-echo "define('DB_NAME', '"$DBNAME"');"                 >>../$FOLDER/wp/wp-config.php
-echo "define('DB_USER', '"$DBUSER"');"                 >>../$FOLDER/wp/wp-config.php
-echo "define('DB_PASSWORD', '"$DBPASS"');"             >>../$FOLDER/wp/wp-config.php
-echo "define('DB_HOST', '"$DBHOST"');"                 >>../$FOLDER/wp/wp-config.php
-echo "define('WP_SITEURL', 'http://"$DOMAIN"');"       >>../$FOLDER/wp/wp-config.php
-echo "define('WP_HOME', 'http://"$DOMAIN"');"          >>../$FOLDER/wp/wp-config.php
-echo "\$table_prefix  = 's3curityy_gr4hl13_';"         >>../$FOLDER/wp/wp-config.php
-
-echo ""                                                >>../$FOLDER/wp/wp-config.php
-
-# Set salts
-curl -s https://api.wordpress.org/secret-key/1.1/salt/ >>../$FOLDER/wp/wp-config.php
-
-echo "define('WP_DEFAULT_THEME', 'grahlie');"          >>../$FOLDER/wp/wp-config.php
-echo "define('DB_CHARSET', 'utf8mb4');"                >>../$FOLDER/wp/wp-config.php
-echo "define('DB_COLLATE', '');"                       >>../$FOLDER/wp/wp-config.php
-echo "define('FS_METHOD', 'direct');"                  >>../$FOLDER/wp/wp-config.php
-
-echo "# Disable edits & updates"                       >>../$FOLDER/wp/wp-config.php
-echo "define('DISALLOW_FILE_EDIT', true);"             >>../$FOLDER/wp/wp-config.php
-echo "define('DISALLOW_FILE_MODS', true);"             >>../$FOLDER/wp/wp-config.php
-echo "define('AUTOMATIC_UPDATER_DISABLED', true);"     >>../$FOLDER/wp/wp-config.php
-echo "define('WP_AUTO_UPDATE_CORE', false);"           >>../$FOLDER/wp/wp-config.php
-
-echo "# Other settings"                                >>../$FOLDER/wp/wp-config.php
-echo "define('WP_AUTO_UPDATE_CORE', false);"           >>../$FOLDER/wp/wp-config.php
-echo "define('AUTOMATIC_UPDATER_DISABLED', true);"     >>../$FOLDER/wp/wp-config.php
-echo "define('DISALLOW_FILE_EDIT', true);"             >>../$FOLDER/wp/wp-config.php
-echo "define('WP_MEMORY_LIMIT', '96M');"               >>../$FOLDER/wp/wp-config.php
-echo "define('WP_POST_REVISIONS', 3);"                 >>../$FOLDER/wp/wp-config.php
-
-echo "if ( !defined('ABSPATH') )"                      >>../$FOLDER/wp/wp-config.php
-echo "  define('ABSPATH', dirname(__FILE__) . '/');"   >>../$FOLDER/wp/wp-config.php
-
-echo "require_once(ABSPATH . 'wp-settings.php');"      >>../$FOLDER/wp/wp-config.php
